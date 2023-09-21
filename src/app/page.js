839671cf4c406b8e51b12e693af6986768bf16b4
nightmarelullaby <videotuto32@gmail.com>
2023-09-21 03:19:55 -0400

    fixed api url

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
     const letterCount = getLetterCount(input)
     setMessages(prev => [...prev,{lettersCount:letterCount,wordCount:wordCount,content:input,sender:true,author:"User",date:moment().format('LT')}])
     setInput("")
-    const req =  await fetch("http://localhost:3000/api/summarize",{
+    const req =  await fetch("/api/summarize",{
       headers:{
         "Content-Type":"application/json"
       },

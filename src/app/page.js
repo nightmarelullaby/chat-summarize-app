ff3bbc7ec59033659d953c2f4a81f22ea996290c
nightmarelullaby <videotuto32@gmail.com>
2023-09-21 08:46:02 -0400

    scroll to bottom added

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,13 @@
 import moment from "moment"
 import {User,Input,Textarea,Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
 import {useState} from "react"
+import {useChatScroll} from "./hooks/useChatScroll"
 import ChatMessage from "./components/ChatMessage"
 export default function Home() {
   const [input,setInput] = useState("")
   const [messages,setMessages] = useState([])
+  const ref = useChatScroll(messages)
+  console.log(ref)
   const getLetterCount = (input) => {
     return input.length
   }
@@ -40,16 +43,16 @@ export default function Home() {
   }
 
   return (
-    <main className=" relative">
-        <div className="fixed z-30 p-2 w-full top-0 bg-neutral-800">
+    <main ref={ref} className="relative overflow-hidden h-screen">
+        <div className="h-fit-content fixed z-30 p-2 w-full top-0 bg-neutral-800">
          <User   
-          name="AI Bot"
-          description="I'm here to help you"
+          name="Resumenes de texto"
+          description="Introduce un texto debajo"
           avatarProps={{
-            src: "https://preview.redd.it/chat-gpt-creates-an-avatar-of-itself-v0-eh6hv5g4v4va1.jpg?width=640&crop=smart&auto=webp&s=e9f753a4ab7782dc40d67ea0fb72151bf2a46559"
+            src: "https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
           }}/>
         </div>
-        <div className="pt-2 px-2 mt-[63px] mb-[48px] flex flex-col gap-2">
+        <div  className="pt-2 px-2 mt-[63px] mb-[48px] flex flex-col gap-2">
           {messages.map(message => <ChatMessage lettersCount={message.lettersCount} wordsCount={message.wordCount}sender={message.sender} author={message.author} date={message.date}content={message.content}/>)}
        </div>
       <form onSubmit={handleSubmit}>
@@ -61,7 +64,7 @@ export default function Home() {
           classNames={{
             label: "text-black/50 dark:text-white/90",
             input: [
-              "bg-transparent py-4",
+              "bg-black py-6 h-8",
               "text-black/90 dark:text-white/90",
               "placeholder:text-default-700/50 dark:placeholder:text-white/60",
             ],
@@ -69,10 +72,8 @@ export default function Home() {
             inputWrapper: [
               "shadow-xl",
               "rounded-b-none",
-              "bg-default-200/50",
+              "bg-black",
               "dark:bg-default/60",
-              "backdrop-blur-xl",
-              "backdrop-saturate-200",
               "hover:bg-default-200/70",
               "dark:hover:bg-default/70",
               "group-data-[focused=true]:bg-default-200/50",
